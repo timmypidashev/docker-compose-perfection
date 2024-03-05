5c2431e07588deaa9d0524c35cce967e0cf21291
Timothy Pidashev <pidashev.tim@gmail.com>
2024-03-05 10:40:25 -0800

    Add api

---
+++ src/api/index.js
@@ -0,0 +1,30 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Here's your response from the node.js 🐳 container",
+  });
+});
+
+app.get("/test", (req, res) => {
+  if (req.body) {
+    alert(req.body);
+    res.send({
+      message:
+        "Here's your response from test URL in the node.js 🐳 container with this in the request body",
+    });
+  } else {
+    res.send({
+      message:
+        "Here's your response from test URL in the node.js 🐳 container with nothing in the request body",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Hi there, I'm listening on port http://localhost:${port}`);
+});
